92c6665b9dff53505ee4384bb92f91afcb48d359
Nihar Joshi <nsj0596@gmail.com>
2021-12-04 01:44:35 -0600

    Added comments
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- src/main/scala/GenerateLogData.scala
+++ src/main/scala/GenerateLogData.scala
@@ -21,9 +21,7 @@ import com.amazonaws.services.s3.AmazonS3Client
 import java.nio.file.Paths
 import java.io.File
 import scala.collection.JavaConverters.*
-
 import java.util.Base64
-
 import com.redis._
 
 
@@ -51,22 +49,34 @@ object GenerateLogData:
     case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
   }
 
+  // Getting encoded AWS credentials
   val AWS_ACCESS_KEY_ENCODED = config.getString("awsConfig.AWS_ACCESS_KEY_ENCODED")
   val AWS_SECRET_KEY_ENCODED = config.getString("awsConfig.AWS_SECRET_KEY_ENCODED")
+
+  // Decoding AWS credentials and converting them to strings
   val AWS_ACCESS_KEY_DECODED = Base64.getDecoder().decode(AWS_ACCESS_KEY_ENCODED)
   val AWS_SECRET_KEY_DECODED = Base64.getDecoder().decode(AWS_SECRET_KEY_ENCODED)
   val AWS_ACCESS_KEY = new String(AWS_ACCESS_KEY_DECODED)
   val AWS_SECRET_KEY = new String(AWS_SECRET_KEY_DECODED)
+
+  // Getting AWS bucket name
   val bucket = config.getString("awsS3updater.bucket")
+
+  // Connecting ot AWS S3 using credentials
   val AWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
   val amazonS3Client = new AmazonS3Client(AWSCredentials)
 
+  // Getting Redis host and port
   val REDIS_HOST = config.getString("awsConfig.REDIS_HOST")
   val REDIS_PORT = config.getInt("awsConfig.REDIS_PORT")
+
+  // Connecting to Redis
   val r = new RedisClient(REDIS_HOST, REDIS_PORT)
 
+  // Getting all generated log files
   val logfiles = java.nio.file.Files.walk(Paths.get("log")).iterator().asScala.filter(file => file.toString.endsWith(".log")).toList
 
+  // Reading log files and putting logs into Redis
   logfiles.foreach(
     logfile => {
       val logs = scala.io.Source.fromFile(logfile.normalize.toString).getLines.toList
@@ -75,8 +85,10 @@ object GenerateLogData:
           r.set("p-" + java.util.UUID.randomUUID.toString, log)
         }
       )
+
       // Optional push log file to S3 bucket
       // amazonS3Client.putObject(bucket, logfile.toString.substring(4), logs.mkString("\r\n"))
       new File(logfile.toString).delete()
+
     }
   )
